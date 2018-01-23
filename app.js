f9dbe706c80dd6ad79fa808fdd3aad83f8e1c18b
austincmatteson <austin.c.matteson@gmail.com>
2018-01-22 17:26:12 -0800

    constructor function for product objects and instances created

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 'use strict';
+
+Product.allProducts = [];
+
+function Product(filepath, name){
+  this.filepath = filepath;
+  this.name = name;
+  this.shown = 0;
+  this.clicked = 0;
+  Product.allProducts.push(this);
+}
+
+new Product('img/bag.jpg', 'bag');
+new Product('img/banana.jpg', 'banana');
+new Product('img/bathroom.jpg', 'bathroom');
+new Product('img/chair.jpg', 'chair');
+new Product('img/cthulhu.jpg', 'cthulhu');
+new Product('img/dog-duck.jpg', 'dog-duck');
+new Product('img/dragon.jpg', 'dragon');
+new Product('img/pen.jpg', 'pen');
+new Product('img/pet-sweep.jpg', 'pet-sweep');
+new Product('img/scissors.jpg', 'scissors');
+new Product('img/shark.jpg', 'shark');
+new Product('img/sweep.jpg', 'sweep');
+new Product('img/tauntaun.jpg', 'tauntaun');
+new Product('img/unicorn.jpg', 'unicorn');
+new Product('img/usb.gif', 'usb');
+new Product('img/water-can.jpg', 'water-can');
+new Product('img/wine-glass.jpg', 'wine-glass');
