46ed2574cfb628bc3140bdf7a2c0bba7bca88fb1
austincmatteson <austin.c.matteson@gmail.com>
2018-01-23 08:55:13 -0800

    show all three, count all three, however vote counting broken

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 'use strict';
 
 Product.allProducts = [];
-var currentIndex = 0;
+var imagesDisplayed = 3;
+var currentImages = [];
+var pic = 0;
 
 function Product(filepath, name){
   this.filepath = filepath;
@@ -33,26 +35,60 @@ new Product('img/water-can.jpg', 'water-can');
 new Product('img/wine-glass.jpg', 'wine-glass');
 
 var imgEl1 = document.getElementById('prod-pic1');
-// var imgEl2 = document.getElementById('prod-pic2');
-// var imgEl3 = document.getElementById('prod-pic3');
+var imgEl2 = document.getElementById('prod-pic2');
+var imgEl3 = document.getElementById('prod-pic3');
 
 function randomProduct() {
-  var randomIndex = Math.floor(Math.random() * Product.allProducts.length);
-  imgEl1.src = Product.allProducts[randomIndex].filepath;
-  Product.allProducts[randomIndex].shown++;
-  console.log(Product.allProducts[randomIndex].name + ' shown: ' + Product.allProducts[randomIndex].shown + ' time(s)');
-  currentIndex = randomIndex;
+  var randomIndex = 0;
+  for(var i = 1; i <= imagesDisplayed; i++) {
+    if(i === 1) {
+      randomIndex = Math.floor(Math.random() * Product.allProducts.length);
+      imgEl1.src = Product.allProducts[randomIndex].filepath;
+      Product.allProducts[randomIndex].shown++;
+      console.log(Product.allProducts[randomIndex].name + ' shown: ' + Product.allProducts[randomIndex].shown + ' time(s)');
+      currentImages.push(randomIndex);
+    } else if (i === 2) {
+      randomIndex = Math.floor(Math.random() * Product.allProducts.length);
+      imgEl2.src = Product.allProducts[randomIndex].filepath;
+      Product.allProducts[randomIndex].shown++;
+      console.log(Product.allProducts[randomIndex].name + ' shown: ' + Product.allProducts[randomIndex].shown + ' time(s)');
+      currentImages.push(randomIndex);
+    } else {
+      randomIndex = Math.floor(Math.random() * Product.allProducts.length);
+      imgEl3.src = Product.allProducts[randomIndex].filepath;
+      Product.allProducts[randomIndex].shown++;
+      console.log(Product.allProducts[randomIndex].name + ' shown: ' + Product.allProducts[randomIndex].shown + ' time(s)');
+      currentImages.push(randomIndex);
+    }
+  }
 }
 
 function count() {
-  Product.allProducts[currentIndex].clicked++;
-  console.log(Product.allProducts[currentIndex].name + ' clicked: ' + Product.allProducts[currentIndex].clicked + ' time(s)');
+  if(pic === 1) {
+    Product.allProducts[currentImages[0]].clicked++;
+    console.log(Product.allProducts[currentImages[0]].name + ' clicked: ' + Product.allProducts[currentImages[0]].clicked + ' time(s)');
+  } else if (pic === 2) {
+    Product.allProducts[currentImages[1]].clicked++;
+    console.log(Product.allProducts[currentImages[1]].name + ' clicked: ' + Product.allProducts[currentImages[1]].clicked + ' time(s)');
+  } else {
+    Product.allProducts[currentImages[2]].clicked++;
+    console.log(Product.allProducts[currentImages[2]].name + ' clicked: ' + Product.allProducts[currentImages[2]].clicked + ' time(s)');
+  }
+  currentImages = [];
 }
 
-imgEl1.addEventListener('click', count);
-imgEl1.addEventListener('click', randomProduct);
+function whichPic() {
+  pic = parseInt(document.getElementById(this));
+}
 
-// imgEl2.addEventListener('click', randomProduct);
-// imgEl3.addEventListener('click', randomProduct);
+imgEl1.addEventListener('click', whichPic);
+imgEl2.addEventListener('click', whichPic);
+imgEl3.addEventListener('click', whichPic);
+imgEl1.addEventListener('click', count);
+imgEl2.addEventListener('click', count);
+imgEl3.addEventListener('click', count);
+imgEl1.addEventListener('click', randomProduct);
+imgEl2.addEventListener('click', randomProduct);
+imgEl3.addEventListener('click', randomProduct);
 
 randomProduct();
